a900364807310d974bdba1a3e16cefb6a50a842d
Craigson <craigpick@gmail.com>
2018-01-19 15:00:03 -0500

    made it async

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # node-pdfsigner
+
+A Javascript library that electronically signs and dates a PDF document for Node.
+
+## Requirements
+
+Node-pdfsigner relies on devongovett's npm package [pdfkit](https://github.com/devongovett/pdfkit) and PDF Labs' [PDFtk Server](https://www.pdflabs.com/tools/pdftk-server/) CLI tool PDFtk Server.
+
+## Installation
+
+Installation uses the [npm](http://npmjs.org/) package manager.  Just type the following command after installing npm.
+
+    npm install pdfkit
+
+You'll need to follow the instructions from PDF labs [here](https://www.pdflabs.com/tools/pdftk-server/) to install <I>dftk</I>.
+
+To install on Ubuntu Linux:
+
+
+    sudo apt-get install pdftk
+
+
+## License
+
+PDFKit is available under the MIT license.
--- index.js
+++ index.js
@@ -3,8 +3,6 @@ TODO:
 - change margin as an option
 */
 
-// Command: pdftk <input pdf> stamp <watermark pdf> output <output filename>
-
 /*
 	options = {
 		name: 'your name',
@@ -13,36 +11,58 @@ TODO:
 		filename: 'filename.pdf'
 	}
 */
-console.log('loading pdfSigner')
-
 const spawn = require('child_process').spawn;
 const fs = require('fs')
 const PDFDocument = require('pdfkit')
 
-function signPdf(original, options, callback)
+
+
+// needs to return watermark filename
+async function createWatermarkPdf(name, ip){
+
+	console.log('execute createWatermarkPdf')
+
+	let date = new Date()
+	let filenameArr = name.split('') // <<< add error checking here
+	let stampFile = filenameArr.join('_') + '_watermark.pdf'
+	const doc = new PDFDocument({margin:0})
+	doc.fontSize(8)
+	doc.text("Signed by: " + name, 50, 750)
+	doc.text("IP: " + ip, 150, 750)
+	doc.text("Signed on: " + date, 380, 750)
+	doc.pipe( fs.createWriteStream(stampFile))
+	doc.end()
+
+	return stampFile
+
+}
+
+// Command: pdftk <input pdf> stamp <watermark pdf> output <output filename>
+// TODO: need to handle the case where options is an empty object
+async function stampPdf(original, stamp, options)
 {
-	if (!options) {
-		options = {};
-	  } else if (typeof options == 'function') {
-		callback = options;
-		options = {};
-	}
+	console.log('execute stampPdf')
 
-	var args = [signPdf.command];
-
-	// this logic could use some better error handling
-	createWatermarkPdf(options.name, options.ip)
-	.then( watermark => {
-
+	try {
+		let args =[signPdf.command]
+		let outputFilename
+	
 		args.push(original)
 		args.push('stamp')
-		args.push(watermark)
+		args.push(stamp)
 		args.push('output')
-		// TODO add check for .pdf extension
-		if (options.filename) args.push(options.filename)
-		else args.push('result.pdf')
-
-		console.log('successfully created watermark')
+	
+		// check for filename option and if it includes the .pdf extension
+		if (options.filename) {
+			if (options.filename.indexOf('.pdf') < 0) outputFilename = options.filename + '.pdf'
+			else outputFilename = options.filename
+		} else {
+			outputFilename = 'output.pdf'
+		}
+	
+		args.push(outputFilename)
+	
+		// create a shell and run the pdftk CLI tool
 		if (process.platform === 'win32') {
 			var child = spawn(args[0], args.slice(1));
 		} else if (process.platform === 'darwin') {
@@ -54,9 +74,9 @@ function signPdf(original, options, callback)
 			console.log('Executing command: ' + args.join(' '))
 			var child = spawn(signPdf.shell, ['-c', args.join(' ') + ' | cat ; exit ${PIPESTATUS[0]}']);
 		}
-
+	
 		var stream = child.stdout;
-
+	
 		// call the callback with null error when the process exits successfully
 		child.on('exit', function(code) {
 			if (code !== 0) {
@@ -64,32 +84,16 @@ function signPdf(original, options, callback)
 			handleError(stderrMessages);
 			} else if (callback) {
 			console.log('child exiting, executing callback')
-			callback(null, stream); // stream is child.stdout
+			return outputFilename;
 			}
 		});
 	
 		// setup error handling
 		var stderrMessages = [];
+	
 		function handleError(err) {
 			var errObj = null;
 			if (Array.isArray(err)) {
-				// check ignore warnings array before killing child
-				if (options.ignore && options.ignore instanceof Array) {
-				var ignoreError = false;
-				options.ignore.forEach(function(opt) {
-					err.forEach(function(error) {
-					if (typeof opt === 'string' && opt === error) {
-						ignoreError = true;
-					}
-					if (opt instanceof RegExp && error.match(opt)) {
-						ignoreError = true;
-					}
-					});
-				});
-				if (ignoreError) {
-					return true;
-				}
-				}
 				errObj = new Error(err.join('\n'));
 			} else if (err) {
 				errObj =  new Error(err);
@@ -98,10 +102,6 @@ function signPdf(original, options, callback)
 			child.kill();
 			// call the callback if there is one
 	
-			if (callback) {
-				callback(errObj);
-			}
-	
 			// if not, or there are listeners for errors, emit the error event
 			if (!callback || stream.listeners('error').length > 0) {
 				stream.emit('error', errObj);
@@ -109,46 +109,49 @@ function signPdf(original, options, callback)
 		}
 	
 		child.once('error', function(err) {
-		throw new Error(err); // critical error
+			throw new Error(err); // critical error
 		});
 	
 		child.stderr.on('data', function(data) {
-		stderrMessages.push((data || '').toString());
-		if (options.debug instanceof Function) {
-			options.debug(data);
-		} else if (options.debug) {
-			console.log('[node-wkhtmltopdf] [debug] ' + data.toString());
-		}
 		});
+
+	} catch (err) {
+
+		return err
+
+	}
 	
-		return stream;
 
-	})
+} // end of stampPdf
 
+async function signPdf(original, options){
 
+	console.log('executing signPdf')
 
+	let name, ip
+	// determine if the 
+	if (!options) {
+		options = {};
+		name = "Unknown"
+		ip = "Unkown"
+	} else {
+		name = options.name
+		ip = options.ip
+	}
 
-} // end of signPdf
+	try {
+		// generate the watermark with the signature
+		const signatureStamp = await createWatermarkPdf(name, ip)
+
+		// stamp the pdf
+		return await stampPdf(original, signatureStamp, options)
+		
+	} catch (err) {
+		logger.error(err)
+	}
+}
+
 
 signPdf.command = 'pdftk'
 signPdf.shell = '/bin/bash'
 module.exports = signPdf
-
-
-function createWatermarkPdf(name, ip){
-
-	return new Promise( (resolve, reject) => {
-		let date = new Date()
-		console.log('date ', date)
-		const doc = new PDFDocument({margin:0})
-		doc.fontSize(8)
-		doc.text("Signed by: " + name, 50, 750)
-		doc.text("IP: " + ip, 150, 750)
-		doc.text("Signed on: " + date, 380, 750)
-		doc.pipe( fs.createWriteStream('watermark.pdf'))
-		doc.end()
-		resolve('watermark.pdf')
-	})
-
-}
-
